53228705a20c6764e3f0ae9ed2147e99009d7c34
Toni Verbeiren <toni.verbeiren@gmail.com>
2024-07-23 15:30:29 +0200

    Avoid using deprecated Viash variable

--- src/concat_text/script.sh
+++ src/concat_text/script.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-TMPDIR=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXX")
+TMPDIR=$(mktemp -d "$meta_temp_dir/concat_text-XXXXXX")
 function clean_up {
   [[ -d "$TMPDIR" ]] && rm -r "$TMPDIR"
 }
