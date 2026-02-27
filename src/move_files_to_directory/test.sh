0719c66593656678f0eb054ca5e269bea4861970
Jakub Majercik <57993790+jakubmajercik@users.noreply.github.com>
2026-02-27 13:56:31 +0100

    move_files_to_directory: allow directories to be copied (#17)

--- src/move_files_to_directory/test.sh
+++ src/move_files_to_directory/test.sh
@@ -11,7 +11,7 @@ trap clean_up EXIT
 touch "$TMPDIR/test_file.txt"
 touch "$TMPDIR/another_file.txt"
 
-./move_files_to_directory \
+$meta_executable \
   --input "$TMPDIR/test_file.txt" \
   --input "$TMPDIR/another_file.txt" \
   --output "$TMPDIR/test_output"
@@ -19,9 +19,23 @@ touch "$TMPDIR/another_file.txt"
 [[ ! -d "$TMPDIR/test_output" ]] && echo "It seems no output directory is generated" && exit 1
 [[ ! -f "$TMPDIR/test_output/test_file.txt" ]] && [[ ! -f test_output/another_file.txt ]] && echo "Output files were not copied to the output directory" && exit 1
 
+# Test copying a directory
+mkdir -p "$TMPDIR/test_dir"
+touch "$TMPDIR/test_dir/file_in_dir.txt"
+touch "$TMPDIR/test_dir/another_file_in_dir.txt"
+
+$meta_executable \
+  --input "$TMPDIR/test_dir" \
+  --output "$TMPDIR/test_output_dir"
+
+[[ ! -d "$TMPDIR/test_output_dir" ]] && echo "It seems no output directory (test_output_dir) is generated" && exit 1
+[[ ! -d "$TMPDIR/test_output_dir/test_dir" ]] && echo "Input directory was not copied to the output directory" && exit 1
+[[ ! -f "$TMPDIR/test_output_dir/test_dir/file_in_dir.txt" ]] && echo "Files inside the copied directory are missing" && exit 1
+[[ ! -f "$TMPDIR/test_output_dir/test_dir/another_file_in_dir.txt" ]] && echo "Files inside the copied directory are missing" && exit 1
+
 ln -s "$TMPDIR/test_file.txt" "$TMPDIR/symlink.txt"
 
-./move_files_to_directory \
+$meta_executable \
   --input "$TMPDIR/symlink.txt" \
   --output "$TMPDIR/test_output_symlink" \
   --keep_symbolic_links
